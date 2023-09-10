df89ed2bc1290473d5221ca0602f111094b7544d
FiammaMuscari <fiammamuscari@gmail.com>
2023-09-10 05:47:20 -0300

    fix: zindex form sidemenu

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -36,7 +36,7 @@ const MobileNav = () => {
         </svg>
       </button>
       <div
-        className={`fixed left-0 top-0 z-10 h-full w-full transform opacity-95 dark:opacity-[0.98] bg-white duration-300 ease-in-out dark:bg-gray-950 ${
+        className={`fixed left-0 top-0  h-full w-full transform opacity-95 z-50 dark:opacity-[0.98] bg-white duration-300 ease-in-out dark:bg-gray-950 ${
           navShow ? 'translate-x-0' : 'translate-x-full'
         }`}
       >
--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -40,7 +40,7 @@ export default function AuthorLayout({ children, content }: Props) {
               <SocialIcon kind="twitter" href={twitter} />
             </div>
           </div>
-          <div className="prose max-w-none pb-8 pt-8 dark:prose-invert xl:col-span-3">
+          <div className="prose max-w-none pb-8 pt-8 dark:prose-invert xl:col-span-3 text-center md:text-left">
             {children}
           </div>
         </div>
